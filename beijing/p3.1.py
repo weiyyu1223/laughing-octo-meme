b760b18055ae18ed53fd4d676c09c0ef19abbf0d
weiyyu1223 <2780822674@qq.com>
2026-08-14 13:44:27 +0800

    Add files via upload

---
+++ beijing/p3.1.py
@@ -0,0 +1,41 @@
+# # # import numpy as np
+# # # a = np.array([1,2,3])
+# # # print(a)
+# # #
+# # import cv2
+# #
+# # # 读取图片（把 'test.jpg' 换成你电脑上有的图片路径）
+# # img = cv2.imread('29b9f4253ef230a917c81132297f45a.jpg')
+# #
+# # # 显示图片
+# # cv2.imshow('My Image', img)
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
+#
+#
+# # import matplotlib.pyplot  as plt
+# # fig=plt.figure()
+# # x = [1,2,3,4,5,6]
+# # y=[1,2,2,3,4,5]
+# # plt.plot(x, y,'cx:')         #红色、破折线、实心圈标记
+# # plt.show()
+#
+#
+# # import numpy as np
+# # import matplotlib.pyplot as plt
+# # x = np.linspace(-10, 10, 100)  #列举出一百个数据点
+# # y = np.cos(x)                           #计算出对应的y
+# # plt.plot(x, y, marker="o")
+# # plt.show()
+#
+#
+# import matplotlib.pyplot as plt
+# import numpy as np
+# a = np.arange(10)
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.plot(a,a*1.5,'b-',a,a*2.5,'r.-',a,a*3.5,'g--',a,a*4.5,'m-')
+# plt.legend(['1.5x','2.5x','3.5x','4.5x'])
+# plt.title('simple lines')
+# plt.show()
+
